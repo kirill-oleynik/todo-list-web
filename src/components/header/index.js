2327f13c2331493ace3db3e1b7e706a102e39d84
KirillØleynik <kirill.olejnik@gmail.com>
2020-10-22 11:35:42 +0300

    Add application title

---
+++ src/components/header/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Title from './title';
+import './header.css';
+
+const Header = () => {
+  return(
+  <div className='header'>
+    <Title />
+  </div>
+  );
+};
+
+export default Header;
---
+++ src/components/header/title/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import './title.css';
+
+const Title = () => (<h1 className='title'>SIMPLE TODO LISTS</h1>);
+
+export default Title;
