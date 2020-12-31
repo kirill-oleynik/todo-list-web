87597259069e901c4ee547f42c336becbbfb6ed2
kirilloleynik <kirill.olejnik@gmail.com>
2020-12-31 19:02:26 +0200

    add todo list buttom compotent template

---
+++ src/components/add-todo-list-button/add-todo-list-button.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './add-todo-list-button.css';
+
+const AddTodoListButton = () => (
+  <div className='add-todo-list-brn-container'>
+  <button className='add-todo-list-button'>
+  + Add TODO List
+  </button>
+</div>
+);
+export default AddTodoListButton;
--- src/components/body/body.js
+++ src/components/body/body.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './body.css';
 import TodoList from '../todo-list';
+import AddTodoListButton from '../add-todo-list-button';
 
 const Body =() => (
   <div className='body'>
   <TodoList />
+  <AddTodoListButton />
   </div>
 );
 export default Body;
