5f5a52dd4f95a1a4b8700279d0534d945d136bd2
kirilloleynik <kirill.olejnik@gmail.com>
2021-01-11 21:54:22 +0200

    update tasks css

--- src/components/task/task.js
+++ src/components/task/task.js
@@ -21,14 +21,14 @@ import {ImAttachment} from 'react-icons/im';
   </div>
   <div className="task-title col-xs-3">{title}</div>
   <div className="col-xs-3 contols">
-        <ImAttachment className='controls add-attachment' />
-        <span className='controls-divider'>|</span>
-        <HiArrowNarrowUp className='controls arrow-up' />
-        <HiArrowNarrowDown className='controls arrow-down' />
-        <span className='controls-divider'>|</span>
-        <TiPencil className='controls edit' />
-        <span className='controls-divider'>|</span>
-        <BsTrash className='controls delete' />
+        <ImAttachment className='task-controls add-attachment' />
+        <span className='task-controls-divider'>|</span>
+        <HiArrowNarrowUp className='task-controls arrow-up' />
+        <HiArrowNarrowDown className='task-controls arrow-down' />
+        <span className='task-controls-divider'>|</span>
+        <TiPencil className='task-controls edit' />
+        <span className='task-controls-divider'>|</span>
+        <BsTrash className='task-controls delete' />
   </div>
 </div>
   </li>
