e1f23f9b730f54feeb492d4b07fd00db04948efe
kirilloleynik <kirill.olejnik@gmail.com>
2021-01-11 22:15:48 +0200

    update add task form layout

--- src/components/add-task-form/add-task-form.js
+++ src/components/add-task-form/add-task-form.js
@@ -4,13 +4,13 @@ import {ImPlus} from 'react-icons/im';
 
 const AddTaskForm = () => {
   return(
-    <div className='add-task-form'>
+    <div className='add-task-form align-middle'>
     <div class="input-group input-group-sm mb-3">
     </div>
     <div class="input-group-prepend">
     <ImPlus className='task-plus-icon' />
     <input placeholder='Start typing here to create task...' type="text" className="form-control" aria-label="Small" aria-describedby="inputGroup-sizing-sm"></input>
-      <button class="btn" type="button">Add task</button>
+      <button class="btn btn-sm add-task-button" type="button">Add Task</button>
     </div>
     </div>
   );
