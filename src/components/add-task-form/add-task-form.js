44a3b0a27b8a8bdbc503e7e8509eed523819f43e
kirilloleynik <kirill.olejnik@gmail.com>
2021-01-11 22:03:39 +0200

    add plus icon to new task form

--- src/components/add-task-form/add-task-form.js
+++ src/components/add-task-form/add-task-form.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './add-task-form.css';
+import {ImPlus} from 'react-icons/im';
 
 const AddTaskForm = () => {
   return(
@@ -7,6 +8,7 @@ const AddTaskForm = () => {
     <div class="input-group input-group-sm mb-3">
     </div>
     <div class="input-group-prepend">
+    <ImPlus className='task-plus-icon' />
     <input placeholder='Start typing here to create task...' type="text" className="form-control" aria-label="Small" aria-describedby="inputGroup-sizing-sm"></input>
       <button class="btn" type="button">Add task</button>
     </div>
